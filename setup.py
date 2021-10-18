d7935a25a8b61ed5e6477295e2087ee8ea6e5f39
Steve Pothier <pothiers@gmail.com>
2021-10-18 07:48:49 -0700

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt") as f:
 
     setuptools.setup(
     name="sparclclient",
-    version="0.3.6",
+    version="0.3.7",
     author="NOIRLab DataLab",
     author_email="datalab@noirlab.edu",
     description="A client for getting spectra data from NOIRLab.",
