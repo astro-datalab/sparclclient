8006bec2f43e5ece10211ae9a15a4ac5314acf79
Alice Jacques <ajacques@sparcdev2.csdc.noirlab.edu>
2022-11-04 12:49:40 -0700

    fix flake8 E128 error

--- tests/expected.py
+++ tests/expected.py
@@ -4,38 +4,38 @@
 idfld = 'id'      # Science Field Name for uuid. Diff val than Internal name.
 
 all_fields = ['MJD',
- 'data_release',
- 'datasetgroup',
- 'dateobs',
- 'dateobs_center',
- 'dec',
- 'dirpath',
- 'exptime',
- 'extra_files',
- 'filename',
- 'filesize',
- 'flux',
- 'id',
- 'instrument',
- 'ivar',
- 'mask',
- 'model',
- 'ra',
- 'redshift',
- 'redshift_err',
- 'redshift_warning',
- 'site',
- 'sky',
- 'specid',
- 'specprimary',
- 'spectype',
- 'targetid',
- 'telescope',
- 'updated',
- 'wave_sigma',
- 'wavelength',
- 'wavemax',
- 'wavemin']
+              'data_release',
+              'datasetgroup',
+              'dateobs',
+              'dateobs_center',
+              'dec',
+              'dirpath',
+              'exptime',
+              'extra_files',
+              'filename',
+              'filesize',
+              'flux',
+              'id',
+              'instrument',
+              'ivar',
+              'mask',
+              'model',
+              'ra',
+              'redshift',
+              'redshift_err',
+              'redshift_warning',
+              'site',
+              'sky',
+              'specid',
+              'specprimary',
+              'spectype',
+              'targetid',
+              'telescope',
+              'updated',
+              'wave_sigma',
+              'wavelength',
+              'wavemax',
+              'wavemin']
 
 default_fields = ['flux', 'id', 'wavelength']
 
@@ -102,9 +102,9 @@ find_0 = [{'_dr': 'SDSS-DR16',
 #  'ra': 248.1914}]
 
 find_1 = [{'_dr': 'BOSS-DR16',
-  'dec': 44.143862,
-  'id': '00000ce3-d15b-4ef5-952b-5790e96af5d7',
-  'ra': 194.55856}]
+           'dec': 44.143862,
+           'id': '00000ce3-d15b-4ef5-952b-5790e96af5d7',
+           'ra': 194.55856}]
 
 #find_2 = 640  # DEV
 find_2 = 936894  # PAT
@@ -123,22 +123,22 @@ find_2 = 936894  # PAT
 #  'ra': 247.39428}]
 
 find_3 = [{'_dr': 'BOSS-DR16',
-  'dec': 44.143862,
-  'id': '00000ce3-d15b-4ef5-952b-5790e96af5d7',
-  'ra': 194.55856},
- {'_dr': 'BOSS-DR16',
-  'dec': 21.33438,
-  'id': '00001f94-96b1-4f2d-8d1d-1a47a89fe105',
-  'ra': 0.863617040000008},
- {'_dr': 'SDSS-DR16',
-  'dec': 62.875655,
-  'id': '00007a88-5add-406d-a0a0-3826c96919a1',
-  'ra': 153.01152}]
+           'dec': 44.143862,
+           'id': '00000ce3-d15b-4ef5-952b-5790e96af5d7',
+           'ra': 194.55856},
+          {'_dr': 'BOSS-DR16',
+           'dec': 21.33438,
+           'id': '00001f94-96b1-4f2d-8d1d-1a47a89fe105',
+           'ra': 0.863617040000008},
+          {'_dr': 'SDSS-DR16',
+           'dec': 62.875655,
+           'id': '00007a88-5add-406d-a0a0-3826c96919a1',
+           'ra': 153.01152}]
 
 #find_4 = ['000547a1-8536-4b47-937b-2f595710fe09',
 #          '01af637c-2d98-4902-b2d3-076a06a01dbd',
 #          '01ba6c0d-2588-4f4e-830e-e68f9f718cf7']
 
 find_4 = ['00000ce3-d15b-4ef5-952b-5790e96af5d7',
- '00001f94-96b1-4f2d-8d1d-1a47a89fe105',
- '00007a88-5add-406d-a0a0-3826c96919a1']
+          '00001f94-96b1-4f2d-8d1d-1a47a89fe105',
+          '00007a88-5add-406d-a0a0-3826c96919a1']
