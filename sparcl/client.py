09c2aa7b0c5b7a8c92496253f5264f87436e458b
Steve Pothier <pothiers@gmail.com>
2023-02-28 13:09:06 -0700

    fixed dls-364

--- sparcl/client.py
+++ sparcl/client.py
@@ -320,7 +320,7 @@ class SparclClient():  # was SparclApi()
 
     def find(self, outfields=None, *,
              constraints={},  # dict(fname) = [op, param, ...]
-             dataset_list=None,
+             #! dataset_list=None,
              limit=500,
              sort=None):
         """Find records in the SPARC database.
@@ -339,11 +339,6 @@ class SparclClient():  # was SparclApi()
                 database subject to restrictions imposed by the ``limit``
                 parameter.
 
-            dataset_list (:obj:`list`, optional): List of data sets from
-                which to find records. Defaults to None, which
-                will find records in all data sets hosted on the SPARC
-                database.
-
             limit (:obj:`int`, optional): Maximum number of records to
                 return. Defaults to 500.
 
@@ -361,6 +356,12 @@ class SparclClient():  # was SparclApi()
             >>> found.records
 
         """
+        # dataset_list (:obj:`list`, optional): List of data sets from
+        #         which to find records. Defaults to None, which
+        #         will find records in all data sets hosted on the SPARC
+        #         database.
+
+
 
         # Let "outfields" default to ['id']; but fld may have been renamed
         if outfields is None:
@@ -373,8 +374,7 @@ class SparclClient():  # was SparclApi()
                        )
                 raise ex.NoCommonIdField(msg)
             outfields = [idfld]
-        if dataset_list is None:
-            dataset_list = self.fields.all_drs
+        dataset_list = self.fields.all_drs
         self._validate_science_fields(outfields, dataset_list=dataset_list)
         dr = list(dataset_list)[0]
         if len(constraints) > 0:
